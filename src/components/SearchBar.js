66ed786a56a2aeada8ec3235db87bb3af1d218c6
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-14 16:54:26 -0400

    search bar result is saved in state and results array gets populated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar(){
+    const [isSearchWord, setIsSearchWord] = React.useState('')
+    const [isResult, setIsResult] = React.useState([])
+
+
+    function handleSearch(e){
+        e.preventDefault()
+        setIsSearchWord(e.target[0].value);
+        
+        retrieveData(e.target[0].value)
+    }
+
+
+    function retrieveData(word){
+        fetch(`https://restcountries.com/v3.1/all`)
+        .then(res => res.json())
+        .then(data => data.map(place => {
+            if (place.name.common.toLowerCase().includes(word.toLowerCase())) {
+                setIsResult((prev => {
+                    return [
+                        ...prev,
+                        place.name.common
+                    ]
+                }))
+            } else {
+                return isResult;
+            }
+        }))
+    }
+
+    console.log(isResult);
+
     return(
-        <div>
-            MAI
+        <div className='search'>
+            <form onSubmit={handleSearch}>
+            <input type='text'></input>
+            <button>Search</button>
+            </form>
         </div>
     )
 }
