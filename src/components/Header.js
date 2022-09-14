1444747439008e67a0f71d91ad3a0942960c7d49
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-14 14:48:07 -0400

    added background image and header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header(){
+
+    return(
+        <div className='head'>
+            <h1>Info Land</h1>
+            <h3>About</h3>
+        </div>
+    )
+}
+
+
+export default Header
