547d233799323b6cf2cde1ace78edf60d1f1b6ec
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-20 13:22:20 -0400

    open in google maps button added to country info

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 function CountryInfo(props){
     
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
+    console.log(props.countryData.map)
+
     return(
         <div className='country-info'>
             <div className='country-info-head'>
             <img src={props.countryData.flag} className='flag'/>
-            <h1>{props.countryData.name}</h1>
+            <h1 className='country-name'>{props.countryData.name}</h1>
             </div>
             <h4>Capital: {props.countryData.capital}</h4>
             <h4>Region: {props.countryData.region}</h4>
             <h4>Sub Region: {props.countryData.subRegion}</h4>
-            <h4>population: {props.countryData.population}</h4>
-            {/* <iframe src={props.countryData.map} width="600" height="450" style={{border: 0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
-
+            <h4>population: {numberWithCommas(props.countryData.population)}</h4>
+            <button className='map-btn'><a href={props.countryData.map}  target='_blank'>Open in Google Maps</a></button>
         </div>
     )
 }
-
 
 export default CountryInfo
