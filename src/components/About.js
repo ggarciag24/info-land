3df5a7631d17c4c6f229d8a6dde88dbad025df6f
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-20 11:53:05 -0400

    email fixed and about pagge content styled

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 function About(){
     return(
-        <div>
-            About page
+        <div className='about'>
+            <h2> About this Project</h2>
+            <p> The purpose of this project is for people to acuire information about any country that they are interested in learning about. Also this project was built to showcase my understanding of React, includine managing state, making fetch calls to an open API and displaying the data conditionally.</p>
+
+            <h4>Contact Information</h4>
+            <p>Email: <a href='mailto: ggarciag.business@outlook.com'>ggarciag.business@outlook.com</a></p>
+            <p>Github: <a href='https://github.com/ggarciag24/'>https://github.com/ggarciag24/</a></p>
         </div>
     )
 }
