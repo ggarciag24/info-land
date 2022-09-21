0affb46835ceb11fe1ffc8277f0171b8ae920846
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-09-21 15:33:44 -0400

    the filter and sort work in <AllCountries />

--- src/components/AllCountries.js
+++ src/components/AllCountries.js
@@ -3,30 +3,92 @@ import CountryCard from './CountryCard';
 
 
 function AllCountries(){
-    const [isCountries, setIsContries] = React.useState([])
+    const [isCountries, setIsCountries] = React.useState([]);
+    const [isSort, setIsSort] = React.useState('alphabetical');
+    const [isFilter, setIsFilter] = React.useState('all');
 
 React.useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/all`)
+        fetch(`https://restcountries.com/v3.1/all`)
         .then(res => res.json())
-        .then(data => setIsContries(data))
+        .then(data => setIsCountries(data))
 },[])
 
-const countryElement = isCountries.map((place, index) => {
-    return <CountryCard name={place.name.common} 
+
+const filteredArr = isCountries.filter((place) => {
+    if(isFilter === 'all'){
+        return true;
+    } else {
+        return place.region === isFilter;
+    }
+})
+
+
+function sortCountries(){
+        if(isSort === 'alphabetical'){
+            filteredArr.sort(function(a, b){
+                if(a.name.common < b.name.common) { return -1; }
+                if(a.name.common > b.name.common) { return 1; }
+                return 0;
+            })
+        }
+        if(isSort === 'mostPopulated'){
+            return filteredArr.sort((a,b) => b.population - a.population)
+        }
+        if(isSort === 'leastPopulated'){
+            return filteredArr.sort((a,b) => a.population - b.population)
+        }
+        return filteredArr;
+    }
+
+sortCountries(); 
+
+
+const countryElement = filteredArr.map((place, index) => {
+    return <CountryCard key={index}
+        name={place.name.common} 
         capital={place.capital}
         region={place.region}
         subRegion={place.subregion}
         flag={place.flags.svg}
         map={place.maps.googleMaps}
-        population={place.population}
-        key={index}/>
+        population={place.population}/>
 })
 
+function handleSort(e){
+    setIsSort(e.target.value);
+}
+
+
+ 
+function handleFilter(e){
+    setIsFilter(e.target.value);
+}
     return(
+        <div>
+            <label>Filter by Region: 
+            <select value={isFilter} onChange={handleFilter}>
+                <option value="all">All</option>
+                <option value="Americas">Americas</option>
+                <option value="Europe">Europe</option>
+                <option value="Africa">Africa</option>
+                <option value="Asia">Asia</option>
+                <option value="Oceania">Oceania</option>
+                <option value="Antarctic">Antarctic</option>
+            </select>
+            </label>
+            <label>Sort by: 
+            <select onChange={handleSort}>
+                <option value="alphabetical">Alphabetical</option>
+                <option value="mostPopulated">most populated</option>
+                <option value="leastPopulated">least populated</option>
+            </select>
+            </label>
+
         <div className='all-countries-container'>
             <div>
             {countryElement}
             </div>
+        </div>
         </div>
     )
 }
