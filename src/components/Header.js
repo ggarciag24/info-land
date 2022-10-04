f16d4c3bce9863b3ba8ce87b20fb4d4bbe504e83
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-10-04 09:30:44 -0400

    all countires link has been removed from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ function Header(){
     return(
         <div className='head'>
             <Link to='/' className='link'><h1>Info Land</h1></Link>
-            <Link to='/all' className='link'><h3>All Countries</h3></Link>
             <Link to='/about' className='link'><h3>About</h3></Link>
         </div>
     )
